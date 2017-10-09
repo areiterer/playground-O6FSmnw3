ec8d50918ff7fd9a9ac56b17dbda57fa8cd13b80
Andreas Reiterer <reiterer.a@gmail.com>
2017-10-09 21:10:02 +0200

    add timestamp to demo data

--- react-project/src/main.js
+++ react-project/src/main.js
@@ -8,21 +8,24 @@ const items = [
     rank: 1,
     title: "How to build a simple HackerNews feed with styled-components",
     url:
-      "http://www.andreasreiterer.at/web-development/styled-components-hackernews-feed/"
+      "http://www.andreasreiterer.at/web-development/styled-components-hackernews-feed/",
+    time: "1507576048"
   },
   {
     id: 124,
     rank: 2,
     title: "React Functional Components vs. Class Components",
     url:
-      "http://www.andreasreiterer.at/web-development/react-functional-components/"
+      "http://www.andreasreiterer.at/web-development/react-functional-components/",
+      time: "1507576048"
   },
   {
     id: 125,
     rank: 3,
     title: "Validating Props easily with React PropTypes",
     url:
-      "http://www.andreasreiterer.at/web-development/react-proptypes/"
+      "http://www.andreasreiterer.at/web-development/react-proptypes/",
+      time: "1507576048"
   }
 ];
 
