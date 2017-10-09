139399b3fa8d84ec0bfcbbe6d8ae989107d11fa0
Andreas Reiterer <reiterer.a@gmail.com>
2017-10-09 20:36:19 +0200

    show StoryList in snippet

--- react-project/src/main.js
+++ react-project/src/main.js
@@ -1,5 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Story from './components/Story';
+import React from "react";
+import ReactDOM from "react-dom";
+import Story from "./components/Story";
 
-ReactDOM.render(<Story />, document.getElementById('App'));
+const items = [
+  {
+    id: 123,
+    rank: 1,
+    title: "How to build a simple HackerNews feed with styled-components",
+    url:
+      "http://www.andreasreiterer.at/web-development/styled-components-hackernews-feed/"
+  },
+  {
+    id: 124,
+    rank: 2,
+    title: "React Functional Components vs. Class Components",
+    url:
+      "http://www.andreasreiterer.at/web-development/react-functional-components/"
+  },
+  {
+    id: 125,
+    rank: 3,
+    title: "Validating Props easily with React PropTypes",
+    url:
+      "http://www.andreasreiterer.at/web-development/react-proptypes/"
+  }
+];
+
+ReactDOM.render(<StoryList items={items} />, document.getElementById("App"));
